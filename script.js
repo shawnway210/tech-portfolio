b99af48e0d102d9c5a140eff5168b1912c4ab9ed
shawnconway <shawnway210@gmail.com>
2023-12-27 13:21:45 -0500

    edited Content

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This website showcases projects I am working on or have finished."
+h2.textContent = "My Portfolio"
 document.querySelector("body").appendChild(h2);
